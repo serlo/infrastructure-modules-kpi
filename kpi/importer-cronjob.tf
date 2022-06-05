bc68d14cd8758b4dc566a0f2da516dc2cad0399e
Jonas Keinholz <git@inyono.dev>
2022-06-05 09:24:43 +0200

    Update providers

--- kpi/importer-cronjob.tf
+++ kpi/importer-cronjob.tf
@@ -44,11 +44,11 @@ resource "kubernetes_deployment" "mysql-importer-cronjob" {
             value = "0 5 * * *"
           }
           resources {
-            limits {
+            limits = {
               cpu    = "50m"
               memory = "64M"
             }
-            requests {
+            requests = {
               cpu    = "25m"
               memory = "32M"
             }
@@ -76,23 +76,5 @@ resource "kubernetes_deployment" "mysql-importer-cronjob" {
         }
       }
     }
-  }
-}
-
-data "template_file" "mysql_importer_config_template" {
-  template = file("${path.module}/mysql-importer-config.yaml.tpl")
-
-  vars = {
-    mysql_importer_interval_in_min = var.mysql_importer_interval_in_min
-    mysql_importer_log_level       = var.mysql_importer_log_level
-    athene2_db_host                = var.athene2_database_host
-    athene2_db_user                = var.athene2_database_username_readonly
-    athene2_db_password            = var.athene2_database_password_readonly
-    athene2_db_name                = var.athene2_database_name
-    kpi_database_host              = var.kpi_database_host
-    kpi_database_port              = 5432
-    kpi_database_name              = var.kpi_database_name
-    kpi_database_username          = var.kpi_database_username_default
-    kpi_database_password          = var.kpi_database_password_default
   }
 }
